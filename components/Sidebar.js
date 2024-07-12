3154bb50068d3407249df76d333dd56dbdb2e661
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-07-12 18:06:39 +0530

    .

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -206,7 +206,7 @@ const Sidebar = () => {
                     type="button"
                     className="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-300 dark:hs-accordion-active:text-white"
                     >
-                    Today's Attendance
+                    Today&apos;s Attendance
                     </button>
                     </Link>
 
