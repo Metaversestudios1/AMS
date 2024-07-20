847636acc66fe86dcdca4066749f601d3141f6ed
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-07-20 17:22:13 +0530

    .

--- app/dashboard/users/add/page.js
+++ app/dashboard/users/add/page.js
@@ -1,112 +1,268 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Page = () => {
+  const [loader, setLoader] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const router = useRouter();
+  const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    designation: "",
+    contact: "",
+  };
+  const [data, setData] = useState(initialState);
+  const validatePhoneNumber = (number) => {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(number);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validatePhoneNumber(contact)) {
+      setErrorMessage('Please enter a valid Phone Number.');
+      return;
+    } else {
+      setErrorMessage('');
+      // Submit the form or handle the valid input
+      console.log('Phone number is valid:', contact);
+    }
+    setLoader(true);
+    const res = await fetch("/api/putusers", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const response = await res.json();
+    if (response.success) {
+      setLoader(false);
+      router.push("/dashboard/users");
+    }
+  };
   return (
-    <div className="w-[70%] m-auto my-10">
-      <form>
-        <div className="grid gap-6 mb-6 md:grid-cols-2">
-          <div>
-            <label
-              htmlFor="full_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="full_name"
-              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-              placeholder="John"
-              required
-            />
-          </div>
-          
-          <div>
-            <label
-              htmlFor="phone"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Phone number
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-              placeholder="123-45-678"
-              required
-            />
-          </div>
-          
-          
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Email address
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-            placeholder="john.doe@company.com"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="designation"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Designation
-          </label>
-          <input
-            type="text"
-            id="designation"
-            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-            placeholder=""
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-            placeholder="•••••••••"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlhtmlFor="confirm_password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-          >
-            Confirm password
-          </label>
-          <input
-            type="password"
-            id="confirm_password"
-           className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-            placeholder="•••••••••"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <>
+      {loader ? (
+        <div
+          aria-label="Loading..."
+          role="status"
+          class="flex items-center justify-center space-x-2 mx-5"
         >
-          ADD
-        </button>
-      </form>
-    </div>
+          <svg
+            class="h-12 w-12 animate-spin stroke-gray-500"
+            viewBox="0 0 256 256"
+          >
+            <line
+              x1="128"
+              y1="32"
+              x2="128"
+              y2="64"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="60.1"
+              x2="173.3"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="224"
+              y1="128"
+              x2="192"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="195.9"
+              x2="173.3"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="128"
+              y1="224"
+              x2="128"
+              y2="192"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="195.9"
+              x2="82.7"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="32"
+              y1="128"
+              x2="64"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="60.1"
+              x2="82.7"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+          </svg>
+        </div>
+      ) : (
+        <div className="w-[70%] m-auto my-10">
+          <form>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                >
+                  Full Name
+                </label>
+                <input
+                  name="name"
+                  value={data.name}
+                  onChange={handleChange}
+                  type="text"
+                  id="name"
+                  className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                  placeholder="John"
+                  required
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="contact"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                >
+                  Phone number
+                </label>
+                <input
+                  name="contact"
+                  value={data.contact}
+                  onChange={handleChange}
+                  type="tel"
+                  id="contact"
+                  className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                  placeholder="123-45-678"
+                  required
+                />
+              </div>
+              {errorMessage && (
+            <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+              {errorMessage}
+            </p>
+          )}
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+              >
+                Email address
+              </label>
+              <input
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+                type="email"
+                id="email"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                placeholder="john.doe@company.com"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="designation"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+              >
+                Designation
+              </label>
+              <input
+                name="designation"
+                value={data.designation}
+                onChange={handleChange}
+                type="text"
+                id="designation"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                placeholder=""
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+              >
+                Password
+              </label>
+              <input
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                type="password"
+                id="password"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                placeholder="•••••••••"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="confirm_password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+              >
+                Confirm password
+              </label>
+              <input
+                name="confirmPassword"
+                value={data.confirmPassword}
+                onChange={handleChange}
+                type="password"
+                id="confirm_password"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
+                placeholder="•••••••••"
+                required
+              />
+            </div>
+            <button
+              onClick={handleSubmit}
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              ADD
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
--- app/dashboard/users/page.js
+++ app/dashboard/users/page.js
@@ -6,46 +6,47 @@ import { MdDelete } from "react-icons/md";
 import { GrFormView } from "react-icons/gr";
 import { CiEdit } from "react-icons/ci";
 export const Page = () => {
-
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
+  const [loader, setLoader] = useState(true);
 
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      const res = await fetch("/api/getusers")
-      const response  = await res.json()
-      setUsers(response.data)
-    }
-    fetchData()
-  },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/getusers");
+      const response = await res.json();
+      setLoader(false);
+      setUsers(response.data);
+    };
+    fetchData();
+  }, []);
   const handleChange = (e) => {
     const { name, value } = e.target;
     if (name == "search") {
       setSearch(value);
     }
   };
-  const handleDelete = async(e,id) =>{
-    e.preventDefault()
-    const res = await fetch("/api/deleteuser",  { method: "DELETE",
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    const res = await fetch("/api/deleteuser", {
+      method: "DELETE",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ id }),
     });
     if (!res.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const response = await res.json();
-    if(response.success) {
-      const updatedUsers = users.filter((item)=>item.emp_id!=id)
-      setUsers(updatedUsers)
+    if (response.success) {
+      const updatedUsers = users.filter((item) => item.emp_id != id);
+      setUsers(updatedUsers);
     }
-
-  }
+  };
   return (
     <div className="">
-    <div className="flex items-center">
-    <div className="bg-yellow-500 rounded-[5px] ml-5 h-[30px] w-[10px]"></div>
-    <div className="text-xl font-bold mx-2 my-8">Users</div>
-  </div>
+      <div className="flex items-center">
+        <div className="bg-yellow-500 rounded-[5px] ml-5 h-[30px] w-[10px]"></div>
+        <div className="text-xl font-bold mx-2 my-8">Users</div>
+      </div>
       <div className="flex justify-between">
         <div className={` flex items-center`}>
           <input
@@ -69,83 +70,171 @@ export const Page = () => {
           </button>
         </Link>
       </div>
-      <div className="relative overflow-x-auto m-5 mb-0">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500  ">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-200">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Sr no.
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Full name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Contact
-              </th>
-              <th scope="col" className="px-6 py-3">
-                email
-              </th>
-              <th scope="col" className="px-6 py-3">
-                designation
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Joining Date
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              users.map((item, index)=>{
-
-                return (<tr key={item._id} className="bg-white border-b ">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  {index+1}
+      {loader ? (
+        <div
+          aria-label="Loading..."
+          role="status"
+          class="flex items-center justify-center space-x-2 mx-5"
+        >
+          <svg
+            class="h-12 w-12 animate-spin stroke-gray-500"
+            viewBox="0 0 256 256"
+          >
+            <line
+              x1="128"
+              y1="32"
+              x2="128"
+              y2="64"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="60.1"
+              x2="173.3"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="224"
+              y1="128"
+              x2="192"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="195.9"
+              x2="173.3"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="128"
+              y1="224"
+              x2="128"
+              y2="192"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="195.9"
+              x2="82.7"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="32"
+              y1="128"
+              x2="64"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="60.1"
+              x2="82.7"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+          </svg>
+        </div>
+      ) : (
+        <div className="relative overflow-x-auto m-5 mb-0">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500  ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Sr no.
                 </th>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  {item?.name}
+                <th scope="col" className="px-6 py-3">
+                  Full name
                 </th>
-                <td className="px-6 py-4">{item?.contact}</td>
-                <td className="px-6 py-4">{item?.email}</td>
-                <td className="px-6 py-4">{item?.designation}</td>
-                <td className="px-6 py-4">{item?.joining_date}</td>
-                <td className="flex py-2  items-center gap-1">
-                  <Link
-                    href={{
-                      pathname: `/dashboard/users/view/[id]`,
-                      query: { id: 12345 },
-                    }}
-                    as={`/dashboard/users/view/${12345}`}
-                  >
-                    <GrFormView className="text-3xl cursor-pointer" />
-                  </Link>
-                  <Link
-                    href={{
-                      pathname: `/dashboard/users/edit/[id]`,
-                      query: {
-                        id: item.emp_id,
-                      },
-                    }}
-                    as={`/dashboard/users/edit/${item.emp_id}`}
-                  >
-                    <CiEdit className="text-2xl cursor-pointer" />
-                  </Link>
-                  <MdDelete onClick={(e)=>handleDelete(e, item.emp_id)} className="text-2xl cursor-pointer" />
-                </td>
-                 </tr>)
-              })
-            }
-          </tbody>
-        </table>
-      </div>
+                <th scope="col" className="px-6 py-3">
+                  Contact
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  designation
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Joining Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((item, index) => {
+                return (
+                  <tr key={item._id} className="bg-white border-b ">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {index + 1}
+                    </th>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {item?.name}
+                    </th>
+                    <td className="px-6 py-4">{item?.contact}</td>
+                    <td className="px-6 py-4">{item?.email}</td>
+                    <td className="px-6 py-4">{item?.designation}</td>
+                    <td className="px-6 py-4">{item?.joining_date}</td>
+                    <td className="flex py-2  items-center gap-1">
+                      <Link
+                        href={{
+                          pathname: `/dashboard/users/view/[id]`,
+                          query: { id: 12345 },
+                        }}
+                        as={`/dashboard/users/view/${12345}`}
+                      >
+                        <GrFormView className="text-3xl cursor-pointer" />
+                      </Link>
+                      <Link
+                        href={{
+                          pathname: `/dashboard/users/edit/[id]`,
+                          query: {
+                            id: item.emp_id,
+                          },
+                        }}
+                        as={`/dashboard/users/edit/${item.emp_id}`}
+                      >
+                        <CiEdit className="text-2xl cursor-pointer" />
+                      </Link>
+                      <MdDelete
+                        onClick={(e) => handleDelete(e, item.emp_id)}
+                        className="text-2xl cursor-pointer"
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
       <div className="flex flex-col items-center my-10">
         <span className="text-sm text-black ">
           Showing <span className="font-semibold text-black ">1</span> to{" "}
